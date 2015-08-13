84283e2f7cfda05acfbe3f9339dfa72a5b3584ec
Raghu Betina <rbetina@github.com>
2015-08-12 19:17:14 -0500

    Adds root route and link for movies in nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "/", :controller => "movies", :action => "index"
+
   # Routes for the Movie resource:
   # CREATE
   get "/movies/new", :controller => "movies", :action => "new"
