b9dc69be2c2a5758528a1cd97270e48e734591f0
Raghu Betina <rbetina@users.noreply.github.com>
2015-11-10 14:45:24 -0600

    Adds association methods to Movie and clean-up

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,7 @@ class Movie < ActiveRecord::Base
   validates :title, :presence => true, :uniqueness => { :scope => :year }
   validates :year, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1870, :less_than_or_equal_to => 2050 }
   validates :duration, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 2764800, :allow_blank => true }
+
+  belongs_to :director
+  has_many :characters
 end
