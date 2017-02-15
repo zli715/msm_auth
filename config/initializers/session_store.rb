e48cb205bb7a8cf6368e45425f372ae2ba9475bc
Raghu Betina <rbetina@gmail.com>
2017-02-15 13:23:14 -0600

    Change name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_must_see_movies_associations_session'
+Rails.application.config.session_store :cookie_store, key: '_msm_auth_session'
